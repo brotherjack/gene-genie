2a8a0346f3e4bbf625a5dc1216315d552af90ff0
Thomas Adriaan Hellinger <brotherjack1877@gmail.com>
2020-03-06 18:57:03 -0500

    added a roulette wheel selection method that can create a roulette wheel, but cannot select yet

--- lib/selection/roulette_wheel.rb
+++ lib/selection/roulette_wheel.rb
@@ -1,2 +1,20 @@
-module 
+require_relative 'selection'
+
+module Genetic
+  module Selection
+    def self.cumulative_sum(array)
+      sum = [1.0]
+      (1..array.length-1).each { |index| sum << array.slice(index..-1).sum }
+      sum
+    end
+
+    class RouletteWheel < Genetic::Selection::Method
+      attr_reader :wheel
+
+      def initialize(population, to=2)
+        normalized_and_sorted = normalize(population, to).sort.reverse
+        @wheel = Genetic::Selection.cumulative_sum normalized_and_sorted
+      end
+    end
+  end
 end
