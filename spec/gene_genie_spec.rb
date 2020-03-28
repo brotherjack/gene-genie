d1ed5f2803eae809c553465b1170da86661e7e48
Thomas Adriaan Hellinger <brotherjack1877@gmail.com>
2020-03-28 19:56:07 -0400

    added equality and strict equality specs for Gene

--- spec/gene_genie_spec.rb
+++ spec/gene_genie_spec.rb
@@ -15,3 +15,33 @@ RSpec.describe GeneGenie::Population do
     end
   end
 end
+
+RSpec.describe GeneGenie::Gene do
+  let(:gene_nervous_10) { described_class.new :nervous, 10 }
+  let(:gene_nervous_100) { described_class.new :nervous, 100 }
+  let(:gene_sleepy_10) { described_class.new :sleepy, 10 }
+
+  describe '#===' do
+    context 'when both genes have the same trait' do
+      specify { expect(gene_nervous_10 === gene_nervous_100).to be_truthy }
+    end
+
+    context 'when the genes have different traits' do
+      specify { expect(gene_nervous_10 === gene_sleepy_10).to be_falsey }
+    end
+  end
+
+  describe '#==' do
+    context 'when both genes have the sane trait and different values' do
+      specify { expect(gene_nervous_10 == gene_nervous_100).to be_falsey }
+    end
+
+    context 'when the genes have different traits' do
+      specify { expect(gene_nervous_10 == gene_sleepy_10).to be_falsey }
+    end
+
+    context 'when the genes have the same trait and same value' do
+      specify { expect(gene_nervous_10 == described_class.new(:nervous, 10)).to be_truthy }
+    end
+  end
+end
