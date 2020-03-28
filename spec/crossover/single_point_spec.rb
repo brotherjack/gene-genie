f243c4cf13d1a3c95cb774ae0c0223eb63568a17
Thomas Adriaan Hellinger <brotherjack1877@gmail.com>
2020-03-28 13:48:24 -0400

    added SinglePoint#compatible? specs

--- spec/crossover/single_point_spec.rb
+++ spec/crossover/single_point_spec.rb
@@ -5,7 +5,42 @@ RSpec.configure do |c|
 end
 
 RSpec.describe GeneGenie::Crossover::SinglePoint do
-  before(:each) do
-    GeneGenie::Population
+  let(:parent_1) do
+    Chromosome.new([
+      Gene.new(:strength, 18),
+      Gene.new(:dexterity, 16),
+      Gene.new(:constitution, 14),
+      Gene.new(:wisdom, 12),
+      Gene.new(:intellegence, 10),
+      Gene.new(:charisma, 8)
+    ])
+  end
+
+  let(:parent_2) do
+    Chromosome.new([
+      Gene.new(:strength, 8),
+      Gene.new(:dexterity, 10),
+      Gene.new(:constitution, 12),
+      Gene.new(:wisdom, 14),
+      Gene.new(:intellegence, 16),
+      Gene.new(:charisma, 18)
+    ])
+  end
+
+  let(:incompatible_length) do
+    Chromosome.new(Gene.new(:x, 2))
+  end
+
+  let(:crossover) { described_class.new }
+
+  describe '#compatible?' do
+    subject { crossover.send :compatible? }
+    context 'when the lengths of the parent chromosomes do NOT match' do
+      it { is_expected.to be_falsey }
+    end
+
+    context 'when the lengths of the parent chromosomes do match' do
+      it { is_expected.to be_truthy }
+    end
   end
 end
