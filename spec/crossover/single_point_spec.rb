822c3c8ef0e30996089d1b41443fda38b9a1dceb
Thomas Adriaan Hellinger <brotherjack1877@gmail.com>
2020-03-29 10:49:05 -0400

    added additional specs for SinglePoint crossover

--- spec/crossover/single_point_spec.rb
+++ spec/crossover/single_point_spec.rb
@@ -5,6 +5,9 @@ RSpec.configure do |c|
 end
 
 RSpec.describe GeneGenie::Crossover::SinglePoint do
+  Chromosome = GeneGenie::Chromosome
+  Gene = GeneGenie::Gene
+
   let(:parent_1) do
     Chromosome.new([
       Gene.new(:strength, 18),
@@ -27,32 +30,83 @@ RSpec.describe GeneGenie::Crossover::SinglePoint do
     ])
   end
 
-  let(:incompatible_length) do
-    Chromosome.new(Gene.new(:x, 2))
+  let(:one_gene_chromosome) do
+    Chromosome.new([Gene.new(:x, 2)])
   end
 
+  let(:n) { parent_1.genes.length }
+  let(:max_children) { (n - 1) * 2 }
   let(:crossover) { described_class.new }
 
   describe '#compatible?' do
-    subject { crossover.send :compatible? }
     context 'when the lengths of the parent chromosomes do NOT match' do
+      subject { crossover.send :compatible?, [parent_1, one_gene_chromosome] }
       it { is_expected.to be_falsey }
     end
 
     context 'when the lengths of the parent chromosomes do match' do
+      subject { crossover.send :compatible?, [parent_1, parent_2] }
       it { is_expected.to be_truthy }
     end
   end
 
   describe '#recombine' do
     context 'when there are more than two parents' do
-      subject { crossover.recombine(parent_1, parent_2, parent_1) }
-      it {expect{subject}.to raise ArgumentError }
+      subject { crossover.recombine([parent_1, parent_2, parent_1]) }
+      it { expect{subject}.to raise_error ArgumentError }
     end
 
     context 'when there is only one parent' do
-      subject { crossover.recombine parent_1 }
-      it {expect{subject}.to raise ArgumentError }
+      subject { crossover.recombine [parent_1] }
+      it {expect{subject}.to raise_error ArgumentError }
+    end
+
+    context 'when a parent has only one gene' do
+      subject { crossover.recombine [parent_1, one_gene_chromosome] }
+      it { expect{subject}.to raise_error IndexError }
+    end
+
+    context 'when children is over the limit' do
+      subject { crossover.recombine [parent_1, parent_2], 100_000 }
+      it { expect{subject}.to raise_error RangeError }
+    end
+
+    context 'when selected point is at...' do
+      subject { crossover.recombine [parent_1, parent_2] }
+      context '...the beginning of the array' do
+        it do
+          allow(subject.select_point).to receive(:select_point) { 0 }
+          expect{subject}.to raise_error IndexError
+        end
+      end
+
+      context '...the end of the array' do
+        it do
+          allow(subject.select_point).to receive(:select_point) { parent_1.length }
+          expect{subject}.to raise_error IndexError 
+        end
+      end
+
+
+      (1..5).each do |point|
+        context "...#{point}" do
+          describe 'child 1' do
+            it "will have the first parent's genes up to #{point}" do
+              allow(subject.select_point).to receive(:select_point) { point }
+              parent_1.genes.slice(1..point).with_index.satisfy do |gene, index|
+                gene == subject[:child_1].genes[index]
+              end
+            end
+
+            it "will have the second parent's genes after #{point}" do
+              allow(subject.select_point).to receive(:select_point) { point }
+              parent_2.genes.slice(point..n).with_index.satisfy do |gene, index|
+                gene == subject[:child_2].genes[index]
+              end
+            end
+          end
+        end
+      end
     end
   end
 end
