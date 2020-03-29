3383aabd744b4efdfe93882a79a3d405212e2266
Thomas Adriaan Hellinger <brotherjack1877@gmail.com>
2020-03-29 11:20:20 -0400

    added exceptions for SinglePoint#recombine

--- lib/crossover/single_point.rb
+++ lib/crossover/single_point.rb
@@ -1,7 +1,30 @@
+require 'byebug'
 module GeneGenie
   module Crossover
     class SinglePoint
-      def recombine(population)
+      def recombine(parents, children=2)
+        raise ArgumentError, 'Can only have two parents' unless parents.length == 2
+
+        if parents.any? { |parent| parent.genes.length < 2 }
+          raise IndexError, 'Both parents must have at least two genes' 
+        end
+
+        max_children = (parents[0].genes.length - 1) * 2
+        unless children <= max_children
+          raise RangeError, "Maximum number of children is #{max_children}"
+        end
+
+
+      end
+
+      private
+
+      def compatible?(parents)
+        return true unless parents[0].genes.length != parents[1].genes.length
+      end
+
+      def select_point
+        raise NotImplementedError
       end
     end
   end
