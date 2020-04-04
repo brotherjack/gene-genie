1f8c464d4fbd83fb7bc503c73102562a1c67a9a4
Thomas Adriaan Hellinger <brotherjack1877@gmail.com>
2020-04-04 12:23:45 -0400

    Crossover::SinglePoint#select_point raises index error when out of points

--- lib/crossover/single_point.rb
+++ lib/crossover/single_point.rb
@@ -2,19 +2,25 @@ require 'byebug'
 module GeneGenie
   module Crossover
     class SinglePoint
-      def recombine(parents, children=2)
+      attr_reader :max_children, :points
+
+      def recombine(parents, children=2, probability_of_crossover=0.5)
         raise ArgumentError, 'Can only have two parents' unless parents.length == 2
 
         if parents.any? { |parent| parent.genes.length < 2 }
           raise IndexError, 'Both parents must have at least two genes' 
         end
 
-        max_children = (parents[0].genes.length - 1) * 2
+        @max_children = (parents[0].genes.length - 1) * 2
         unless children <= max_children
           raise RangeError, "Maximum number of children is #{max_children}"
         end
 
+        unless compatible? parents
+          raise TypeError, 'Both chromosomes must have the same number of genes'
+        end
 
+        @points = (1...parents[0].genes.length).to_a
       end
 
       private
@@ -24,7 +30,9 @@ module GeneGenie
       end
 
       def select_point
-        raise NotImplementedError
+        raise IndexError, 'No more points!' if @points.length == 0
+        point = @points.sample
+        @points.delete_at @points.index(point)
       end
     end
   end
